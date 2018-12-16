9d3fb4b9726f7fe4ce5001763b68e5176ef30664
Tomy <tomyleereyes@gmail.com>
2018-12-15 16:41:20 -0800

    add google auth

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,11 +3,13 @@ import AddUpdate from './AddUpdate.js'
 import UpdatesList from './UpdatesList.js'
 import { withTracker } from 'meteor/react-meteor-data'
 import { Updates } from '../api/updates.js'
+import AccountsUIWrapper from './AccountsUIWrapper.js'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <AccountsUIWrapper />
         <AddUpdate handleSubmit={this.handleSubmit} />
         <UpdatesList updates={this.props.updates} />
       </div>
